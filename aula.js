75a78ac160a7296c7de3fef99e67840afb5115ff
VICTOR RICARDO CAETANO LIMA <victortrader770@gmail.com>
2022-02-03 06:55:28 -0300

    primeiro commit

---
+++ aula.js
@@ -0,0 +1,219 @@
+//     variaveis
+// let nome = 'victor ricardo';
+// let idade = '22 anos';
+// let altura = 1.80;
+// let peso = '100 kg';
+// console.log(nome);
+// console.log(idade);
+// console.log(altura);
+// console.log(peso);
+
+//     tipos de dados primitivos
+// let nome = 'Rafael'; //string literal
+// let idade = 25; //number literal
+// let estaAprovado = true; //boolean
+// let sobrenome = undefined; //undefined
+// let corSelecionada = null;//redefinir um valor 
+// console.log(estaAprovado)
+// console.log(nome)
+// console.log(corSelecionada)
+
+//     objetos
+// let pessoa = {
+//     nome: 'victor',
+//     idade: 22,
+//     estaAprovado: 'yes',
+//     sobrenome: 'Lima'
+// };
+// console.log(pessoa)
+
+// let turno = {
+//     manha: '8 as 12',
+//     tarde: '13 as 17',
+//     noite: '18 as 22'
+// };
+// console.log(turno)
+
+// let escola = {
+//     nome: 'Andrea de Aguiar',
+//     horarios: 'das 7 da manha as 22 da noite',
+//     matérias: 10,
+//     professores: 20
+// };
+// console.log(escola)
+
+// let trabalho = {
+//     começo: '8hs da manha',
+//     intervalo: '12 horas',
+//     retomada: '14 horas',
+//     largada: '18 horas'
+// };
+// console.log(trabalho)
+
+//      arrays
+
+// let familia = [
+//     20,99,'victor',22,44
+// ];
+// //começa a contar a partir do 0
+// console.log(familia[2]);
+// console.log(familia.length); //quantos parametros dentro de um array
+
+// let colega = [
+//     'nick', '22 anos', 'alto', 'pardo'  
+// ];
+// console.log(colega);
+ 
+//    Function
+// let corCasa = 'amarelo';
+// function mudarTinta(){
+//     corCasa = "branca";
+// };
+// console.log(corCasa);
+// mudarTinta('azul');
+// console.log(corCasa)
+
+//  let corSite = 'verde';
+//  function resetaCor(cor){
+//     corSite = cor;
+//  };
+//  console.log(corSite);
+//  resetaCor('azul');
+//  console.log(corSite)
+// console.log(corSite);
+// resetaCor();
+// console.log(corSite);
+
+// let corSite = 'azul';
+// function resetaCor(cor, tonalidade){
+//     corSite = cor + tonalidade;
+// };
+// console.log(corSite);
+// resetaCor('verde', ' Claro');
+// console.log(corSite);
+
+// let corCarro = 'rosa';
+// function pintarCarro(cor, tipo){
+//     corCarro = cor + tipo;
+// };
+// console.log(corCarro);
+// pintarCarro('preto',' perolizado');
+// console.log(corCarro);
+
+// let corRoupa = 'verde';
+// function trocarCor(cor, tom){
+//     trocarCor = cor + tom;
+// };
+// console.log(corRoupa);
+// trocarCor('azul', ' escuro');
+// console.log(trocarCor);
+
+// function dizerNome(){
+//     console.log('Victor')
+// };
+// dizerNome();
+// function MultiplicarPorDez(valor){
+//     return valor* 10;
+// };
+// console.log(MultiplicarPorDez(20));
+// let res = MultiplicarPorDez(5);
+// console.log(res);
+
+//   Operadores
+
+//   operadores aritimeticos
+//let salario = 100;
+
+// console.log(salario + salario);
+// console.log(salario - salario);
+// console.log(salario / salario);
+// console.log(salario * salario);
+// console.log(10 ** 10);
+
+// operadores de encremento e decremento 
+
+//++ --
+
+// let idade = 22;
+// console.log(idade++);
+// console.log(idade);
+// console.log(++idade);
+// console.log(--idade);
+// console.log(--idade);
+// console.log(--idade);
+// console.log(idade); 
+
+//operadores de atribuição
+//  let valorMac = 5000;
+//  console.log(valorMac);
+//  valorMac += valorMac;
+//  console.log(valorMac);
+//  valorMac -= valorMac;
+//  console.log(valorMac);
+
+//opreradores de igualdade
+// igualdade escrita
+// console.log(1 === 1);
+// console.log('12' === '12');
+// console.log(12 === 9);
+
+//igualdade solta
+// console.log(1 == '1')
+// console.log("12" == 12);
+
+//operador ternario
+
+// let pontos = 200;
+// let tipo = pontos > 100 ? 'premium' : 'comum';
+// console.log(tipo);
+
+// let pontuacao = 2000;
+// let marca = pontuacao > 1300 ? 'master' : 'normal';
+// console.log(marca);
+
+    
+//operadores lógicos 
+
+// operador lógico (&&)
+
+// let maior = true;
+// let possuiCarteira = false;
+// let podeAplicar = maior && possuiCarteira;
+// console.log(podeAplicar);
+
+// operador lógico ou (||)
+
+// let maior = false;
+// let possuiCarteira = false;
+// let podeAplicar = maior || possuiCarteira;
+// console.log('podeAplicar: ' ,podeAplicar);
+
+// operador NOT (!)
+
+
+// let candidatoRecusado = !podeAplicar;
+// console.log('candidato Recusado: ' ,candidatoRecusado);
+
+//operadores de comparação nÃo boleanos
+
+// let corPersonalizada = '';
+// let corPadrao = 'azul';
+// let corSegunda = 'roxo';
+// let corPefil = corPersonalizada || corSegunda;
+// console.log(corPefil);
+// console.log(corPefil);
+
+//operadores bitwise 
+
+// mini projeto
+
+// let a = 'rosa';
+// let b = 'roxo';
+// let c = a;
+
+// a = b;
+// b = c;
+// console.log(a);
+// console.log(b);
+
+
